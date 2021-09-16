5d7931d08726648339bdc790514d7dcca35b6612
Mr. Unknown <58999771+MrUnknownOp@users.noreply.github.com>
2021-09-16 17:41:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const RPC = require('discord-rpc')
+const rpc = new RPC.Client({
+    transport: 'ipc'
+})
+
+rpc.on('ready', async() => {
+    
+    rpc.setActivity({
+        details: 'This is details',
+        state: 'this is state',
+        largeImageKey: 'large',
+        largeImageText: 'This is a large image',
+        smallImageKey: 'small',
+        smallImageText: 'this is small image',
+        startTimestamp: new Date(),
+        buttons: [{
+            label: 'This is b1',
+            url: 'https://discord.com/developers/applications/886948968053997598/information'
+        },{
+           label : 'This is b2',
+           url : 'https://discord.com/developers/applications/886948968053997598/information' 
+        }]
+    })
+    console.log('Client is ready')
+})
+
+rpc.login({
+    clientId: '886948968053997598'
+})
