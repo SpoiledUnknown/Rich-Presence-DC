c7b96095bd1fd08a0c4e9f20f422746445e071b1
Spoiled Unknown <shanjusamsong254@gmail.com>
2024-01-16 21:03:14 +0530

    Updated the rentire project for better user experience.

--- README.md
+++ README.md
@@ -1,7 +1,95 @@
-# Rich-Presence-DC
+# Richcord Presence - Discord RPC
 
-A simple code to make a Rich presence for discord written in javascript.
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+[![Docs Passing](https://img.shields.io/badge/Docs-Passing-brightgreen.svg)](https://github.com/SpoiledUnknown/Richcord-Presence)
 
-Watch This tutorial to understand to use this 
+![Richorc](/assets/Richcord.png)
 
-Youtube Link :- https://youtu.be/ggAjf1liy0k
+## Table Of Content:
+1. [Description](#description)
+2. [Installation](#installation)
+   1. [Method 1](#method-1)
+   2. [Method 2](#method-2)
+3. [Setting up the dev environment](#how-to-setup-dev-environment)
+4. [Creating custom client](#custom-client-setup)
+5. [How to use](#how-to-use)
+
+## Description:
+
+ An advance discord rich presence software that allows a lot of customisation and control in hand of user without over complicating things.
+
+ ## Installation:
+
+***Currently we only support windows 11, more OS supported will be added.***
+
+**Note:** *Method 2 requires the NodeJS installed in pc if not avaibale install from [here](https://nodejs.org/en/download/).*
+
+### Method 1:
+
+- Download the executable from [here]().
+   - Double click the exe file.
+   - Go through the simple prompts.
+ - Note: in some cases the exe file might not work in that case use bellow provided steps -
+    - Open terminal (non-admin)
+    - drag and drop the exe file to the ternimal window
+    - press enter.
+### Method 2:
+- Clone the repo, or download the source code from [here]().
+- Open the ternimal at the location of the source code
+- Use `cd "path/to/inside/of/source-code/"`, to access the source code.
+- Run `npm install`.
+- Run `npm run start` or `node index.js`
+
+## How To Setup Dev Environment:
+- Install NodeJs from [here](https://nodejs.org/en/download/).
+ - Clone the repo, or download the source code from [here]().
+ - Open it using [VS Code](https://code.visualstudio.com/download) or any other Code Editor/IDE.
+- Press `ctrl` + `
+- Run `npm install`
+  
+## Custom Client Setup:
+
+Go to the *[Discord Dev Portal](https://discord.com/developers/applications)*
+
+
+1. Click on tthe ***New Application*** button. 
+![Step 1](/assets/step%201.png)
+2. A new dailogue box appears.
+3. Enter the name of the application (***This shows as the app name in discord status***)
+4. Agree the terms of service.
+5. Click on *create*
+![Step 2](/assets/step%202.png)
+6. Click on *Rich Presence*
+![Step 3](/assets/step%203.png)
+7. Scroll a little and click on *Add Images*
+   - **Note:** *You can only add images more than 512x512 (1024x1024 is recommended).*
+   - **Note:** *You can add upto 300 images.*
+![Step 4](/assets/step%204.png)
+8. Write the name of the asset.
+   - **Note:** *You can't change the name once the asset is saved. If you want to change then delete and reupload the asset.*
+   - **Note:** *Remember the name you gave to the assets as they will have to write it as is while running the app*
+9. Click on *Save Changes* and wait for changes to be saved.
+![Step 5](/assets/step%205.png)
+10. Now again click on *General Information*.
+11. Scroll down a little bit to copy your client id.
+![Step 6](/assets/step%206.png)
+12. Click on *Copy*.
+![Step 7](/assets/step%207.png)
+
+#### The client setup is done, now you can just paste the client id when prompted.
+
+## How To Use:
+1. The app first asks the ***Client ID***, which we have created using above provided method.
+2. You then have to provide a *detail* message. This message will be shown on detail area in discord
+3. Then you have to provide a *status* for your app.
+4.   Then provide the *Large Image Key*, the name of the asset uploaded in custom client setup.
+5.   Then provide a message for Large Image.
+6.   Same step for small image, write the name of small image asset.
+7.   and again a message for small image.
+8.   Then write the label for first button.
+9.   Then give a URL for the first button.
+10.   Follow the above 2 steps for second button as well
+11.   And lastly tell whether you want the timestamps to be shown or not.
+  
+![Example](/assets/example.png)
+#### You are all done, now enjoy the Richcord by flexing on your virtual friends since you most likely don't have any irl friends.
--- index.js
+++ index.js
@@ -1,29 +1,231 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer'
+import chalkAnimation from 'chalk-animation'
+import { createSpinner } from 'nanospinner'
 import RPC from 'discord-rpc'
+
+
+
+const sleep = (ms = 3000) => new Promise((r) => setTimeout(r, ms))
+
+let clientId, state, detail, largeImageKey, largeImageText, smallImageKey, smallImageText,
+buttonKey, buttonURL, secButtonKey, secButtonURL, timestamps
+
+//#region Setup
+
+async function Start()
+{
+  const welcomeText = chalkAnimation.karaoke('Welcome to Richcord Presence!')
+  await sleep()
+  welcomeText.stop()
+  const devName = chalkAnimation.pulse('Made by Spoiled Unknown || version 1.0.2')
+  await sleep()
+  devName.stop()
+}
+
+async function ClientId()
+{
+  const response = await inquirer.prompt({
+    name: 'client_id',
+    type: 'input',
+    message: 'Please enter the client id.',
+    default: '886576833838088243'
+  })
+
+  clientId = response.client_id
+}
+
+async function State()
+{
+  const response = await inquirer.prompt({
+    name: 'state',
+    type: 'input',
+    message: 'Please write the state message.',
+    default: 'Richord Presence || V 1.0.2'
+  })
+
+  state = response.state
+}
+
+async function Detail()
+{
+  const response = await inquirer.prompt({
+    name: 'detail',
+    type: 'input',
+    message: 'Please write the detail message.',
+    default: 'Made By Spoiled Unknown'
+  })
+
+  detail = response.detail
+}
+
+async function LargeImage()
+{
+  const response = await inquirer.prompt({
+    name: 'large_image',
+    type: 'input',
+    message: 'Please write the large image key.',
+    default: 'richcord'
+  })
+
+  largeImageKey = response.large_image
+}
+
+async function LargeText()
+{
+  const response = await inquirer.prompt({
+    name: 'large_text',
+    type: 'input',
+    message: 'Please write the large image text.',
+    default: 'Richcord Presence || Version 1.0.2'
+  })
+
+  largeImageText = response.large_text
+}
+
+async function SmallImage()
+{
+  const response = await inquirer.prompt({
+    name: 'small_image',
+    type: 'input',
+    message: 'Please write the small image key.',
+    default: 'github'
+  })
+
+  smallImageKey = response.small_image
+}
+
+async function SmallText()
+{
+  const response = await inquirer.prompt({
+    name: 'small_text',
+    type: 'input',
+    message: 'Please write the small image text.',
+    default: "Visit richcord's github page to know more"
+  })
+
+  smallImageText = response.small_text
+}
+
+async function FirstButtonText()
+{
+  const response = await inquirer.prompt({
+    name: 'first_text',
+    type: 'input',
+    message: 'Please write the label for button 1.',
+    default: "Repository"
+  })
+
+  buttonKey = response.first_text
+}
+
+async function FirstButtonURL()
+{
+  const response = await inquirer.prompt({
+    name: 'first_url',
+    type: 'input',
+    message: 'Please write the url for button 1.',
+    default: "https://github.com/SpoiledUnknown/Richcord-Presence"
+  })
+
+  buttonURL = response.first_url
+}
+
+async function SecButtonText()
+{
+  const response = await inquirer.prompt({
+    name: 'sec_text',
+    type: 'input',
+    message: 'Please write the label for button 2.',
+    default: "Richcord Community"
+  })
+
+  secButtonKey = response.sec_text
+}
+
+async function SecButtonURL()
+{
+  const response = await inquirer.prompt({
+    name: 'sec_url',
+    type: 'input',
+    message: 'Please write the url for button 2.',
+    default: "https://discord.gg/AUfhhEkhgp"
+  })
+
+  secButtonURL = response.sec_url
+}
+
+async function SetTimestamps()
+{
+  const response = await inquirer.prompt({
+    name: 'timestamps',
+    type: 'confirm',
+    message: 'Should the time stamps be on or off?'
+  })
+
+  if (response.timestamps) 
+  {
+    timestamps = new Date()
+  }
+  else
+  {
+    timestamps = null
+  }
+}
+
+//#endregion
+
+
+console.clear()
+await Start()
+await ClientId()
+await Detail()
+await State()
+await LargeImage()
+await LargeText()
+await SmallImage()
+await SmallText()
+await FirstButtonText()
+await FirstButtonURL()
+await SecButtonText()
+await SecButtonURL()
+await SetTimestamps()
+
+const spinner = createSpinner('Connecting to the client').start()
+
 const richPresence = new RPC.Client({
-    transport:'ipc'
+  transport:'ipc'
 })
 
-richPresence.on('ready', () => {
-    richPresence.setActivity({
-        details: 'This is details',
-        state: 'this is state',
-        startTimestamp: new Date(),
-        largeImageKey: 'unreal',
-        largeImageText: 'This is a large image',
-        smallImageKey: 'visualstudios',
-        smallImageText: 'this is small image',
-        buttons: [{
-            label: 'This is button 1',
-            url: 'https://discord.com/developers/applications/886948968053997598/information'
-        },{
-           label : 'This is button 2',
-           url : 'https://discord.com/developers/applications/886948968053997598/information' 
-        }]
-    })
 
-    console.log("Client is ready!")
+
+
+richPresence.on('ready', async () => {
+
+  await sleep()
+
+  richPresence.setActivity({
+    details: detail,
+    state: state,
+    startTimestamp: timestamps,
+    largeImageKey: largeImageKey,
+    largeImageText: largeImageText,
+    smallImageKey: smallImageKey,
+    smallImageText: smallImageText,
+    buttons: [{
+      label: buttonKey,
+      url: buttonURL
+    },{
+      label : secButtonKey,
+      url : secButtonURL 
+    }]
+  })
+
+  spinner.success({text:'Connected...'})
+  chalkAnimation.radar('Client connected successfully!')
 })
 
 richPresence.login({
-    clientId: '886576833838088243'
+  clientId: clientId
 })
